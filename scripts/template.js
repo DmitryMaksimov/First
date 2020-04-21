f080975475e248ce819be77795972350457c44f6
d_maksimov <d_maksimov@inbox.ru>
2020-04-21 21:26:24 +0500

    Перепилил header на flex

--- scripts/template.js
+++ scripts/template.js
@@ -1,6 +1,6 @@
 function OnResize() {
   /* Берем наше верхнее меню */
-  var header = document.querySelector('header');
+  var header = document.querySelector('.menu__wrapper');
   /* Получаем размер верхнего меню */
   var margin_top = header.clientHeight;
 
@@ -8,11 +8,13 @@ function OnResize() {
   content = document.querySelector('.content_wrapper');
   content.style.marginTop = margin_top + 'px';
   
+  document.querySelector('header').height = margin_top + 'px';
+
   /* При скрулинге вправо/влево надо двигать меню тоже */
   /* Вычисляем размер сдвига по оси x */
-  var offset_left = document.documentElement.scrollLeft || document.body.scrollLeft;
+//  var offset_left = document.documentElement.scrollLeft || document.body.scrollLeft;
   /* Сдвигаем наше верхнее меню */
-  header.style.left = -offset_left + 'px';
+//  header.style.left = -offset_left + 'px';
 //  content.style.marginBottom = bottom + 'px';
 }
 
