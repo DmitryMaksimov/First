14b09060319f444b148dc0ff31b3bb5db6b96366
d_maksimov <d_maksimov@inbox.ru>
2020-04-21 14:17:36 +0500

    Добавил шрифт material icons

--- scripts/template.js
+++ scripts/template.js
@@ -1,9 +1,23 @@
 function OnResize() {
-  var top = document.querySelector('header').clientHeight;
-//  var bottom = document.querySelector('footer').clientHeight;
+  /* Берем наше верхнее меню */
+  var header = document.querySelector('header');
+  /* Получаем размер верхнего меню */
+  var margin_top = header.clientHeight;
+
+  /* Делаем пустой отступ сверху для контента */
   content = document.querySelector('.content_wrapper');
-  content.style.marginTop = top + 'px';
+  content.style.marginTop = margin_top + 'px';
+  
+  /* При скрулинге вправо/влево надо двигать меню тоже */
+  /* Вычисляем размер сдвига по оси x */
+  var offset_left = document.documentElement.scrollLeft || document.body.scrollLeft;
+  /* Сдвигаем наше верхнее меню */
+  header.style.left = -offset_left + 'px';
 //  content.style.marginBottom = bottom + 'px';
+}
+
+window.onscroll = function() {
+  OnResize();
 }
 
 window.onresize = function () {
