c396440aeefa7a3e3c06c226a8eb15561e36c2b5
d_maksimov <d_maksimov@inbox.ru>
2020-05-08 17:47:15 +0500

    Переработал шаблон, верхнее меню и подвал с копирайтами

--- scripts/template.js
+++ scripts/template.js
@@ -1,20 +1,26 @@
 function OnResize() {
+  /* Берем наш content */
+  var content = document.querySelector('main');
   /* Берем наше верхнее меню */
-  var header = document.querySelector('.menu__wrapper');
+  var header_wrapper = document.querySelector('.menu__wrapper');
+
+
+  var header = document.querySelector('header');
+  header.style.width = content.clientWidth + 'px';
+
   /* Получаем размер верхнего меню */
-  var margin_top = header.clientHeight;
+  var margin_top = header_wrapper.clientHeight;
 
   /* Делаем пустой отступ сверху для контента */
-  content = document.querySelector('.content_wrapper');
   content.style.marginTop = margin_top + 'px';
   
   document.querySelector('header').height = margin_top + 'px';
 
   /* При скрулинге вправо/влево надо двигать меню тоже */
   /* Вычисляем размер сдвига по оси x */
-//  var offset_left = document.documentElement.scrollLeft || document.body.scrollLeft;
+  var offset_left = document.documentElement.scrollLeft || document.body.scrollLeft;
   /* Сдвигаем наше верхнее меню */
-//  header.style.left = -offset_left + 'px';
+  header.style.left = (-offset_left) + 'px';
 //  content.style.marginBottom = bottom + 'px';
 }
 
